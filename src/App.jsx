8da2c11fe07685cdfb2a46c5cc9028225d9c1749
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-01 10:35:40 -0300

    agregamos la peticion a la API con fethc

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
 import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
-
+import { useState, useEffect } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    try {
+      // vamos  a hacer una petición get del servidor.
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      const dato = await respuesta.json();
+      console.log(respuesta);
+      console.log(dato[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
         <Frase />
-        <Button variant="warning" >
-          Obtener frase
-        </Button>
+        <Button variant="warning">Obtener frase</Button>
       </Container>
     </>
   );
